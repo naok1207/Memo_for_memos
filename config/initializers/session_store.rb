fae97f409b40e69e40293aaa8a2b8fa2c6ea5223
naok1207 <zenos.game.anime.all.vo1@gmail.com>
2021-05-13 21:40:16 +0900

    fix error session_store
    ruby3.0 へ移行時にエラー発生

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,11 +1,10 @@
-Rails.application.config.session_store :redis_store, {
+Rails.application.config.session_store :redis_store,
   servers: [
       {
-          host: ENV.fetch('REDIS_HOST') { 'localhost' },
-          port: ENV.fetch('REDIS_PORT') { 6379 },
+          host: Settings.redis_host || 'localhost' ,
+          port: Settings.redis_port || 6378,
           db: 0,
           namespace: 'session'
       }
   ],
   expire_after: 90.minutes
-}
