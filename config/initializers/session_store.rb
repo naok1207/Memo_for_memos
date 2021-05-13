d0a50745525053b4ea2406519dcef9ed7cf8ce60
naok1207 <zenos.game.anime.all.vo1@gmail.com>
2021-05-14 07:08:02 +0900

    fix error again session_store redis

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,10 +1,1 @@
-Rails.application.config.session_store :redis_store,
-  servers: [
-      {
-          host: Settings.redis_host || 'localhost' ,
-          port: Settings.redis_port || 6378,
-          db: 0,
-          namespace: 'session'
-      }
-  ],
-  expire_after: 90.minutes
+Rails.application.config.session_store :redis_store, url: Settings.redis.url, namespace: 'session', expire_after: 1.weeks
