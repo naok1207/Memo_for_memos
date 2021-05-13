da0e032018dbca8d373b49b4486aec32ff6a73a5
naok1207 <zenos.game.anime.all.vo1@gmail.com>
2021-05-14 07:29:54 +0900

    add user auth by sorcery

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "sessions#new"
+
+  # 認証
+  get     '/login',  to: "sessions#new"
+  post    '/login',  to: "sessions#create"
+  delete  '/logout',  to: "sessions#destroy"
+
+  # ユーザ登録
+  resources :users
 end
