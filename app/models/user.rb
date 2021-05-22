9e6dbe69c265734b746fbf6223b7d626bcf49e41
naok1207 <zenos.game.anime.all.vo1@gmail.com>
2021-05-22 14:34:25 +0900

    fix user create process

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,7 +34,6 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 8, maximum: 24 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
-  validates :access_token, presence: true, uniqueness: true
 
   has_many :categories, dependent: :destroy
   has_many :memos, dependent: :nullify
