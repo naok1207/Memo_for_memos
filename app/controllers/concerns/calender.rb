dff492e85d52bd7ceb9966f17d28461cf007112e
naok1207 <zenos.game.anime.all.vo1@gmail.com>
2021-05-25 08:28:09 +0900

    edit calnder sql reflash

--- app/controllers/concerns/calender.rb
+++ app/controllers/concerns/calender.rb
@@ -7,5 +7,7 @@ module Calender
     from_date = Date.new(@date.year, @date.month, @date.beginning_of_month.day).beginning_of_week(:sunday)
     to_day = Date.new(@date.year, @date.month, @date.end_of_month.day).end_of_week(:sunday)
     @calendar_data = from_date.upto(to_day)
+    @user = params[:username].present? ? User.find_by(username: params[:username]) : current_user
+    @memo_updated_ats = @user.memos.select("date(updated_at) as date").group("date(updated_at)")
   end
 end
