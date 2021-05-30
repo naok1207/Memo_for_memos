8ac7e16d54f1284974189e94658e0153660e8ac4
naok1207 <zenos.game.anime.all.vo1@gmail.com>
2021-05-30 17:12:11 +0900

    add markdown_helper

---
+++ app/helpers/markdown_helper.rb
@@ -0,0 +1,21 @@
+module MarkdownHelper
+  def markdown(content)
+    return '' unless content.present?
+    @options ||= {
+        filter_html: true,
+        autolink: true,
+        space_after_headers: true,
+        fenced_code_blocks: true,
+        underline: true,
+        highlight: true,
+        footnotes: true,
+        tables: true,
+        hard_wrap: true,
+        xhtml: true,
+        link_attributes: {rel: 'nofollow', target: "_blank"},
+        strikethrough: true
+    }
+    @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, @options)
+    @markdown.render(content).html_safe
+  end
+end
