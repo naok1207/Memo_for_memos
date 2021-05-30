e620f1129e0ba4d81fa4480c5e98b218fe3f9dfe
naok1207 <zenos.game.anime.all.vo1@gmail.com>
2021-05-30 17:24:25 +0900

    add highlight.js for markdown

---
+++ app/javascript/packs/markdown.js
@@ -0,0 +1,7 @@
+import hljs from 'highlight.js';
+import 'highlight.js/styles/github.css';
+document.addEventListener('DOMContentLoaded', (event) => {
+  document.querySelectorAll('pre code').forEach((block) => {
+    hljs.highlightBlock(block);
+  });
+});
