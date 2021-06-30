238bfcd78a9b02558b024bab640899089dad5e01
naok1207 <zenos.game.anime.all.vo1@gmail.com>
2021-06-30 23:32:58 +0900

    add minds with memo

--- app/controllers/users/minds_controller.rb
+++ app/controllers/users/minds_controller.rb
@@ -3,5 +3,7 @@ class Users::MindsController < ApplicationController
 
   def show
     @user = User.find_by!(username: params[:user_username])
+    @categories = @user.categories.pluck(:id, :name, :parent_id).map{ |id, name, parent_id| { id: id, name: name, parent_id: parent_id }}
+    @memos = @user.memos.pluck(:id, :title, :category_id).map{ |id, title, category_id| { id: id, title: title, category_id: category_id }}
   end
 end
